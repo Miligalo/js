4dcd4788169b60333720490f6b2a1ceb4443a6c5
Miligalo <milioglo2000@gmail.com>
2022-03-18 18:13:40 +0200

    JS Laravel

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <CreateComponent></CreateComponent>
+        <SinglePostComponent></SinglePostComponent>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Age</th>
+      <th scope="col">Job</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="person in persons">
+      <th scope="row">{{person.id}}</th>
+      <td>{{person.name}}</td>
+      <td>{{person.age}}</td>
+      <td>{{person.job}}</td>
+    </tr>
+
+  </tbody>
+</table>
+    </div>
+</template>
+
+<script>
+import SinglePostComponent from "./SinglePostComponent"
+import CreateComponent from "./CreateComponent"
+export default {
+    name: "PostComponent",
+
+    data(){
+        return {
+            persons: null
+        }
+    },
+
+    mounted(){
+
+    },
+
+    methods: {
+
+        },
+    
+
+    components: {
+        SinglePostComponent,
+        CreateComponent
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
